e4a93ce94d56cdc7efd84eaaf1ac4e2fedb9ff71
Manuel Araoz <manuelaraoz@gmail.com>
2014-04-06 13:56:32 -0300

    migrating networks.js

--- networks.js
+++ networks.js
@@ -4,43 +4,19 @@ var hex = function(hex) {return new Buffer(hex, 'hex');};
 
 exports.livenet = {
   name: 'livenet',
-  addressVersion: 0x00,
-  magic: hex('f9beb4d9'),
+  addressVersion: 0x1E,
+  magic: hex('c0c0c0c0'),
   genesisBlock: {
     height: 0,
-    nonce: 2083236893,
+    nonce: 99943,
     version: 1,
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
+    hash: hex('1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691'),
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-    bits: 486604799
+    timestamp: 1386325540,
+    merkle_root: hex('5b2a3f53f605d62c53e62932dac6925e3d74afa5a4b459745c36d42d0ed26a69'),
+    bits: 0x1e0ffff0
   },
-  genesisBlockTx: {
-    outs: [{
-      v: hex('00F2052A01000000'), // 50 BTC
-      s: new Put()
-        .word8(65) // 65 bytes of data follow
-        .put(hex('04678AFDB0FE5548271967F1A67130B7105CD6A828E03909A67962E0EA1F61DEB649F6BC3F4CEF38C4F35504E51EC112DE5C384DF7BA0B8D578A4C702B6BF11D5F'))
-        .word8(0xAC) // OP_CHECKSIG
-        .buffer()
-    }],
-    lock_time: 0,
-    version: 1,
-    hash: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-    ins: [{
-      q: 0xFFFFFFFF,
-      o: hex("0000000000000000000000000000000000000000000000000000000000000000FFFFFFFF"),
-      s: new Put()
-        .put(hex('04FFFF001D010445'))
-        .put(new Buffer('The Times 03/Jan/2009 Chancellor on brink of ' +
-                        'second bailout for banks', 'ascii'))
-        .buffer()
-    }]
-  },
-  proofOfWorkLimit: hex("00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"),
-  checkpoints: [], // need to put checkpoint blocks here
-  addressPubkey: 0,
+  addressPubkey: 0x1E,
   addressScript: 5,
   bip32public: 0x0488b21e,
   bip32private: 0x0488ade4,
@@ -50,7 +26,7 @@ exports.livenet = {
 exports.testnet = {
   name: 'testnet',
   addressVersion: 0x6f,
-  magic: hex('0b110907'),
+  magic: hex('fcc1b7dc'),
   genesisBlock: {
     height: 0,
     nonce: 414098458,
